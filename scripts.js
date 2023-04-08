8b40036ed2d7ce1ee587840f2ac7d585b7c1bed9
Ana Beatriz Pires <abmpires1999@gmail.com>
2023-04-08 01:03:01 +0100

    acabei a página sobre mim

--- scripts.js
+++ scripts.js
@@ -1,12 +1,13 @@
 showMenu = function (bars) {
+    
     if (document.getElementsByClassName('menu-container')[0].style.visibility === "visible") {
         document.getElementsByClassName('menu-container')[0].style.visibility = "hidden";
-        bars.classList.toggle("change");
+
     } 
     else {
         document.getElementsByClassName('menu-container')[0].style.visibility = "visible";
-        bars.classList.toggle("change");
     }
+    bars.classList.toggle("change");
 }
 
 
