5cece5189da4c91f52e000f3a38028b7c43cbfbd
Ana Beatriz Pires <abmpires1999@gmail.com>
2023-04-10 16:56:32 +0100

    fiz a página do contact me

--- scripts.js
+++ scripts.js
@@ -1,13 +1,20 @@
-showMenu = function (bars) {
-    
+showMenu = function () {
+    var bar1 = document.querySelector(".bar1");
+    var bar2 = document.querySelector(".bar2");
+    var bar3 = document.querySelector(".bar3");
+
     if (document.getElementsByClassName('menu-container')[0].style.visibility === "visible") {
         document.getElementsByClassName('menu-container')[0].style.visibility = "hidden";
-
+        bar1.classList.remove("change");
+        bar2.classList.remove("change");
+        bar3.classList.remove("change");
     } 
     else {
         document.getElementsByClassName('menu-container')[0].style.visibility = "visible";
+        bar1.classList.add("change");
+        bar2.classList.add("change");
+        bar3.classList.add("change");
     }
-    bars.classList.toggle("change");
 }
 
 
