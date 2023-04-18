849667e6ff66128b724850950a5e081ea64031b3
Ana Beatriz Pires <abmpires1999@gmail.com>
2023-04-18 09:03:24 +0100

    dei fix à calculadora

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ showMenu = function () {
         bar1.classList.remove("change");
         bar2.classList.remove("change");
         bar3.classList.remove("change");
-    } 
+    }
     else {
         document.getElementsByClassName('menu-container')[0].style.visibility = "visible";
         bar1.classList.add("change");
@@ -16,3 +16,22 @@ showMenu = function () {
         bar3.classList.add("change");
     }
 }
+
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+    console.log(button.value)
+    
+        if (button.value == "C") {
+            display.innerText = "";
+        }
+        else if (button.value == "=") {
+            display.innerText = eval(display.innerText) ;
+        }
+        else {
+            display.innerText += button.value;
+        }
+        console.log(display.value)
+    })})
